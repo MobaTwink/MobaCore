f9c96c5d1afa2bdaca5b23f3533f43de653dcd55
kamoulox <internetbidon@gmail.com>
2012-11-23 13:25:51 +0100

    Custom : Updated announce with rating and stuff.

--- src/server/game/Battlegrounds/BattlegroundQueue.cpp
+++ src/server/game/Battlegrounds/BattlegroundQueue.cpp
@@ -220,22 +220,29 @@ GroupQueueInfo* BattlegroundQueue::AddGroup(Player* leader, Group* grp, Battlegr
                         qAlliance, (MinPlayers > qAlliance) ? MinPlayers - qAlliance : (uint32)0, qHorde, (MinPlayers > qHorde) ? MinPlayers - qHorde : (uint32)0);
                 }
 				
+
 			    std::string color((ginfo->Team == HORDE) ? "|cfffa2b2b" : "|cff3898fa");
-			    std::string name("|Hplayer:"+leader->GetName()+"|h"+leader->GetName()+"|h");
+			    std::string name("|Hplayer:"+leader->GetName()+"|h"+leader->GetName()+"|h|r");
                 // System message
 				if (q_max_level == 19)
 				{
 					if (q_min_level == 10)
-						sWorld->SendWorldText(MOBA_BG_QUEU, bgName, color.c_str(), name.c_str(), "|r", qAlliance, qHorde);
+						sWorld->SendWorldText(MOBA_BG_QUEU, bgName, color.c_str(), name.c_str(), qAlliance, qHorde);
 
 					else if (ArenaType == 5)
-						sWorld->SendWorldText(MOBA_ARENA_DUEL,  color.c_str(), name.c_str(), "|r");
+						sWorld->SendWorldText(MOBA_BG_QUEU, "5v5", color.c_str(), name.c_str(), qAlliance, qHorde);
+
+					else if (ArenaType == 2 )
+						sWorld->SendWorldText(MOBA_ARENA_DUEL,  color.c_str(), name.c_str());
 
 					else
-						sWorld->SendWorldText(MOBA_ARENA_SKIRMISH, ArenaType, ArenaType, color.c_str(), name.c_str(), "|r");
+					{
+						ArenaTeam* RatedTeam = sArenaTeamMgr->GetArenaTeamByCaptain(leader->GetGUID());
+						sWorld->SendWorldText(MOBA_ARENA_SKIRMISH, ArenaType, ArenaType, color.c_str(), RatedTeam->GetRating(), name.c_str());
+					}
 				}
 				else
-					sWorld->SendWorldText(MOBA_ARATHI_QUEU, bgName, color.c_str(), name.c_str(), "|r");
+					sWorld->SendWorldText(MOBA_ARATHI_QUEU, bgName, color.c_str(), name.c_str());
 			}
         }
         //release mutex
