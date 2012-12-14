dab10bde6ae145740b7addbc775dc82e13bca3a9
kamoulox <internetbidon@gmail.com>
2012-12-14 23:08:51 +0100

    Custom : Players can only buy 2v2 Teams instead of 3v3

--- src/server/game/Handlers/PetitionsHandler.cpp
+++ src/server/game/Handlers/PetitionsHandler.cpp
@@ -959,17 +959,10 @@ void WorldSession::SendPetitionShowList(uint64 guid)
     {
         data << uint8(3);                                   // count
         // 2v2
-        data << uint32(0);                                  // index
-        data << uint32(0);             // charter entry
-        data << uint32(0);                 // charter display id
-        data << uint32(0);        // charter cost
-        data << uint32(0);                                  // unknown
-        data << uint32(0);                                  // required signs?
-        // 3v3
-        data << uint32(2);                                  // index
-        data << uint32(ARENA_TEAM_CHARTER_3v3);             // charter entry
+        data << uint32(1);                                  // index
+        data << uint32(ARENA_TEAM_CHARTER_2v2);             // charter entry
         data << uint32(CHARTER_DISPLAY_ID);                 // charter display id
-        data << uint32(ARENA_TEAM_CHARTER_3v3_COST);        // charter cost
+        data << uint32(ARENA_TEAM_CHARTER_2v2_COST);        // charter cost
         data << uint32(0);                                  // unknown
         data << uint32(0);                                  // required signs?
     }
