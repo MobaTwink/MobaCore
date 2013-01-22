485b0dfa6321790982a7640971e7552fa03c0c8f
kamoulox <internetbidon@gmail.com>
2013-01-22 17:40:46 +0100

    [Retail] Fixed Arena unit Frames

--- src/server/game/Battlegrounds/BattlegroundMgr.cpp
+++ src/server/game/Battlegrounds/BattlegroundMgr.cpp
@@ -206,12 +206,7 @@ void BattlegroundMgr::BuildBattlegroundStatusPacket(WorldPacket* data, Battlegro
             *data << uint64(0);                             // 3.3.5, unknown
             *data << uint32(Time1);                         // time to bg auto leave, 0 at bg start, 120000 after bg end, milliseconds
             *data << uint32(Time2);                         // time from bg start, milliseconds
-			if(arenaFaction == ALLIANCE) {
-				*data << uint8(1);
-			} else {
-			//if(arenaFaction == HORDE) {
-				*data << uint8(0);
-			}
+			*data << uint8(arenaFaction == ALLIANCE ? 1 : 0); // arenafaction (0 for horde, 1 for alliance)
             break;
         default:
             sLog->outError(LOG_FILTER_BATTLEGROUND, "Unknown BG status!");
